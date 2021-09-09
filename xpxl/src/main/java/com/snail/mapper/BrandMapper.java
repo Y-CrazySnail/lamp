1b4ee08a601679b08af5561eafded03eb5b2d0aa
yanghs@bonree.com <19980720Meng>
2021-09-09 10:15:28 +0800

    xpxl品牌信息查询

--- xpxl/src/main/java/com/snail/mapper/BrandMapper.java
+++ xpxl/src/main/java/com/snail/mapper/BrandMapper.java
@@ -4,8 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.snail.entity.Brand;
 import com.snail.entity.Price;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
-@Mapper
+@Repository
 public interface BrandMapper extends BaseMapper<Brand> {
 
 }
--- xpxl/src/main/java/com/snail/service/impl/BrandServiceImpl.java
+++ xpxl/src/main/java/com/snail/service/impl/BrandServiceImpl.java
@@ -1,15 +1,30 @@
 package com.snail.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.snail.entity.Brand;
 import com.snail.entity.Price;
 import com.snail.mapper.BrandMapper;
 import com.snail.mapper.PriceMapper;
 import com.snail.service.IBrandService;
-import com.snail.service.IPriceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BrandServiceImpl extends ServiceImpl<BrandMapper, Brand> implements IBrandService {
 
+    @Autowired
+    private BrandMapper brandMapper;
+
+    @Override
+    public List<Brand> list() {
+        QueryWrapper<Brand> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("first_char");
+        brandMapper.selectList(queryWrapper);
+        return null;
+    }
 }
