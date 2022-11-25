59105e5b96631bf47825d9b7ed7625b52d98cfae
杨海松 <haisong0230@outlook.com>
2022-11-25 09:50:20 +0800

    AiLi项目升级

--- e-server/src/main/java/com/snail/aili/controller/AiliQualityController.java
+++ e-server/src/main/java/com/snail/aili/controller/AiliQualityController.java
@@ -7,10 +7,7 @@ import com.snail.conreoller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/aili-quality")
@@ -18,6 +15,27 @@ public class AiliQualityController extends BaseController<AiliQuality> {
 
     @Autowired
     private IAiliQualityService qualityService;
+
+    @GetMapping("getPage")
+    public ResponseEntity<Object> getPage(Integer current,
+                                          Integer size,
+                                          String name,
+                                          String phone,
+                                          String checkStatus
+    ) {
+        QueryWrapper<AiliQuality> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByAsc("check_status");
+        if (!StringUtils.isEmpty(name)) {
+            queryWrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(phone)) {
+            queryWrapper.like("phone", phone);
+        }
+        if (!StringUtils.isEmpty(checkStatus)) {
+            queryWrapper.eq("check_status", checkStatus);
+        }
+        return ResponseEntity.ok(super.getPage(current, size, queryWrapper));
+    }
 
     @PostMapping("getByCondition")
     public ResponseEntity<Object> getByCondition(@RequestBody AiliQuality quality) {
@@ -29,6 +47,7 @@ public class AiliQualityController extends BaseController<AiliQuality> {
         } else {
             return ResponseEntity.badRequest().body(null);
         }
+        qualityQueryWrapper.eq("check_status", "1");
         quality = qualityService.getOne(qualityQueryWrapper);
         if (StringUtils.isEmpty(quality)) {
             return ResponseEntity.badRequest().body(null);
--- e-server/src/main/java/com/snail/aili/entity/AiliQuality.java
+++ e-server/src/main/java/com/snail/aili/entity/AiliQuality.java
@@ -2,7 +2,9 @@ package com.snail.aili.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.snail.entity.BaseEntity;
+import lombok.Data;
 
+@Data
 @TableName(value = "aili_quality", autoResultMap = true)
 public class AiliQuality extends BaseEntity {
     /**
@@ -49,92 +51,8 @@ public class AiliQuality extends BaseEntity {
      * 产品级别
      */
     private String productLevel;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCarModel() {
-        return carModel;
-    }
-
-    public void setCarModel(String carModel) {
-        this.carModel = carModel;
-    }
-
-    public String getCarNumber() {
-        return carNumber;
-    }
-
-    public void setCarNumber(String carNumber) {
-        this.carNumber = carNumber;
-    }
-
-    public String getCarShelfNumber() {
-        return carShelfNumber;
-    }
-
-    public void setCarShelfNumber(String carShelfNumber) {
-        this.carShelfNumber = carShelfNumber;
-    }
-
-    public String getCarColor() {
-        return carColor;
-    }
-
-    public void setCarColor(String carColor) {
-        this.carColor = carColor;
-    }
-
-    public String getWorkDateTime() {
-        return workDateTime;
-    }
-
-    public void setWorkDateTime(String workDateTime) {
-        this.workDateTime = workDateTime;
-    }
-
-    public String getWorkCompany() {
-        return workCompany;
-    }
-
-    public void setWorkCompany(String workCompany) {
-        this.workCompany = workCompany;
-    }
-
-    public String getWorkPart() {
-        return workPart;
-    }
-
-    public void setWorkPart(String workPart) {
-        this.workPart = workPart;
-    }
-
-    public String getWorkStaff() {
-        return workStaff;
-    }
-
-    public void setWorkStaff(String workStaff) {
-        this.workStaff = workStaff;
-    }
-
-    public String getProductLevel() {
-        return productLevel;
-    }
-
-    public void setProductLevel(String productLevel) {
-        this.productLevel = productLevel;
-    }
+    /**
+     * 审核状态 0未审核 1已审核
+     */
+    private Integer checkStatus;
 }
