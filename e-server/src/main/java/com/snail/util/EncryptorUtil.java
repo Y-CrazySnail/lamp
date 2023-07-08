854a9191622c29ff9b9b0d54a9c0759b9005df9f
杨海松 <haisong0230@outlook.com>
2023-07-09 02:08:14 +0800

    商品功能

--- e-server/src/main/java/com/snail/util/EncryptorUtil.java
+++ e-server/src/main/java/com/snail/util/EncryptorUtil.java
@@ -4,11 +4,10 @@ import com.baomidou.mybatisplus.core.toolkit.AES;
 
 public class EncryptorUtil {
     public static void main(String[] args) {
-        String key = "cbb5165ec91c8361";
-        System.out.println(key);
-        String username = "tank";
-        String password = "Tank20230620";
-        System.out.println(AES.encrypt(username, key));
-        System.out.println(AES.encrypt(password, key));
+//        System.out.println(AES.generateRandomKey());
+//        String key = "4ae128ddba39376c";
+//        System.out.println(key);
+//        System.out.println(AES.encrypt(username, key));
+//        System.out.println(AES.encrypt(password, key));
     }
 }
