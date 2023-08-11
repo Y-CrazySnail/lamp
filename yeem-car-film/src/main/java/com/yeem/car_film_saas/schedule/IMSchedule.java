d5d536fbb3b4d33e11cff8ed56546e9c1237bd07
QiMou <1270737197@qq.com>
2023-08-11 18:59:55 +0800

    业务调用邮件的save方法把数据拼起来然后放进邮件数据表 之后有个读取方法 读取出来 定时发送

--- yeem-car-film/src/main/java/com/yeem/car_film_saas/schedule/IMSchedule.java
+++ yeem-car-film/src/main/java/com/yeem/car_film_saas/schedule/IMSchedule.java
@@ -1,12 +1,16 @@
 package com.yeem.car_film_saas.schedule;
 
+import cn.hutool.extra.mail.MailUtil;
+import com.yeem.car_film_saas.entity.SysMail;
 import com.yeem.car_film_saas.service.ISysIMService;
+import com.yeem.car_film_saas.service.ISysMailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -15,12 +19,18 @@ public class IMSchedule {
     @Autowired
     private ISysIMService sysIMService;
 
+    @Autowired
+    ISysMailService sysMailService;
     /**
      * 发送邮件定时任务 每10秒执行一次
      */
     @Scheduled(cron = "0/10 * * * * ?")
     public void schedule() {
         log.info("send mail start:{}", LocalDateTime.now());
+        List<SysMail> todoMail = sysMailService.getTodoMail();
+        for (SysMail sysMail : todoMail) {
+            MailUtil.send(sysMail.getToEmail(),sysMail.getSubject(),sysMail.getContent(), sysMail.getAttachment().isEmpty());
+        }
 
         //  todo 定时任务 定时扫描
     }
