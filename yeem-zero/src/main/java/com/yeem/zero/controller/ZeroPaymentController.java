d70ef3de6b109b4ec6e6f5b7f87bf6499bfe4307
杨海松 <haisong0230@outlook.com>
2023-08-14 23:28:13 +0800

    支付功能

--- yeem-zero/src/main/java/com/yeem/zero/controller/ZeroPaymentController.java
+++ yeem-zero/src/main/java/com/yeem/zero/controller/ZeroPaymentController.java
@@ -1,6 +1,11 @@
 package com.yeem.zero.controller;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.wechat.pay.java.core.RSAAutoCertificateConfig;
+import com.wechat.pay.java.core.exception.ValidationException;
+import com.wechat.pay.java.core.notification.NotificationConfig;
+import com.wechat.pay.java.core.notification.NotificationParser;
+import com.wechat.pay.java.service.partnerpayments.jsapi.model.Transaction;
 import com.wechat.pay.java.service.payments.jsapi.model.PrepayWithRequestPaymentResponse;
 import com.yeem.common.conreoller.BaseController;
 import com.yeem.zero.entity.ZeroOrder;
@@ -11,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.PrivateKey;
 import java.util.Map;
 
 @Slf4j
@@ -35,6 +41,44 @@ public class ZeroPaymentController extends BaseController<ZeroOrder> {
     @PostMapping("callback")
     public ResponseEntity<Object> callback(@RequestBody ObjectNode objectNode) {
         try {
+//            // 构造 RequestParam
+//            com.wechat.pay.java.core.notification.RequestParam requestParam =
+//                    new com.wechat.pay.java.core.notification.RequestParam.Builder()
+//                            .serialNumber(wechatPaySerial)
+//                            .nonce(wechatpayNonce)
+//                            .signature(wechatSignature)
+//                            .timestamp(wechatTimestamp)
+//                            .body(requestBody)
+//                            .build();
+//
+//// 如果已经初始化了 RSAAutoCertificateConfig，可直接使用
+//// 没有的话，则构造一个
+//            NotificationConfig config = new RSAAutoCertificateConfig.Builder()
+//                    .merchantId(merchantId)
+//                    .privateKeyFromPath(privateKeyPath)
+//                    .merchantSerialNumber(merchantSerialNumber)
+//                    .apiV3Key(apiV3Key)
+//                    .build();
+//
+//// 初始化 NotificationParser
+//            NotificationParser parser = new NotificationParser(config);
+//
+//            try {
+//                // 以支付通知回调为例，验签、解密并转换成 Transaction
+//                Transaction transaction = (Transaction) parser.parse(requestParam, Transaction.class);
+//            } catch (ValidationException e) {
+//                // 签名验证失败，返回 401 UNAUTHORIZED 状态码
+//                logger.error("sign verification failed", e);
+//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED);
+//            }
+//
+//// 如果处理失败，应返回 4xx/5xx 的状态码，例如 500 INTERNAL_SERVER_ERROR
+//            if (/* process error */) {
+//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
+//            }
+//
+//// 处理成功，返回 200 OK 状态码
+//            return ResponseEntity.status(HttpStatus.OK);
             log.info("支付回调：{}", objectNode);
             return ResponseEntity.ok("");
         } catch (Exception e) {
