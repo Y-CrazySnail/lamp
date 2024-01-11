aaf5c9d90b42b08515a965a590c6f34729175abf
杨海松 <haisong0230@outlook.com>
2024-01-11 21:03:58 +0800

    Zero优化

--- yeem-car/src/main/java/com/yeem/car/entity/CarFilmQuality.java
+++ yeem-car/src/main/java/com/yeem/car/entity/CarFilmQuality.java
@@ -12,34 +12,101 @@ import java.util.Date;
 
 @TableName(value = "car_film_quality")
 public class CarFilmQuality extends BaseEntity {
-    private String name; // 姓名
-    private String phone; // 手机号
-    private String qualityCardNo; // 质保卡号
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 手机号
+     */
+    private String phone;
+    /**
+     * 质保卡号
+     */
+    private String qualityCardNo;
     /**
      * 质保状态
      */
     @TableField(exist = false)
     private String state;
-    private String plateNo; // 车牌号
-    private String vin; // 车架号
-    private String productNo; // 产品代码
-    private String productName;// 产品名称
-    private String productLevelNo; // 产品级别代码
-    private String productLevelName; // 产品级别名称
-    private BigDecimal price; // 价格
-    private String carBrand; // 汽车品牌
-    private String carModel; // 汽车型号
-    private String carColor; // 汽车颜色
+    /**
+     * 车牌号
+     */
+    private String plateNo;
+    /**
+     * 车架号
+     */
+    private String vin;
+    /**
+     * 产品代码
+     */
+    private String productNo;
+    /**
+     * 产品名称
+     */
+    private String productName;
+    /**
+     * 产品级别代码
+     */
+    private String productLevelNo;
+    /**
+     * 产品级别名称
+     */
+    private String productLevelName;
+    /**
+     * 价格
+     */
+    private BigDecimal price;
+    /**
+     * 汽车品牌
+     */
+    private String carBrand;
+    /**
+     * 汽车型号
+     */
+    private String carModel;
+    /**
+     * 汽车颜色
+     */
+    private String carColor;
+    /**
+     * 施工时间
+     */
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date workDate; // 施工时间
+    private Date workDate;
+    /**
+     * 质保有效期
+     */
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date validityDate; // 质保有效期
-    private String workCompany; // 施工单位
-    private String workStaff; // 施工技师
-    private String workPart; // 施工部位
-    private String rollNumber; // 卷心号
-    private String boxNumber; // 盒头号
-    private String approveFlag; //审核标识
+    private Date validityDate;
+    /**
+     * 质保年限
+     */
+    private Integer guaranteePeriod;
+    /**
+     * 施工单位
+     */
+    private String workCompany;
+    /**
+     * 施工技师
+     */
+    private String workStaff;
+    /**
+     * 施工部位
+     */
+    private String workPart;
+    /**
+     * 卷心号
+     */
+    private String rollNumber;
+    /**
+     * 盒头号
+     */
+    private String boxNumber;
+    /**
+     * 审核标识
+     */
+    private String approveFlag;
     @TableField(exist = false)
     private String productType;
 
@@ -153,6 +220,14 @@ public class CarFilmQuality extends BaseEntity {
 
     public void setValidityDate(Date validityDate) {
         this.validityDate = validityDate;
+    }
+
+    public Integer getGuaranteePeriod() {
+        return guaranteePeriod;
+    }
+
+    public void setGuaranteePeriod(Integer guaranteePeriod) {
+        this.guaranteePeriod = guaranteePeriod;
     }
 
     public String getWorkCompany() {
