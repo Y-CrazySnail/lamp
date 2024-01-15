1fed3e2c6f5195d319b778de977c6b592714bbaf
cse_yanghaisong <yanghaisong@csc.com.cn>
2024-01-15 18:03:47 +0800

    Aladdin优化

--- aladdin-lamp/src/main/java/com/yeem/lamp/service/IAladdinServiceService.java
+++ aladdin-lamp/src/main/java/com/yeem/lamp/service/IAladdinServiceService.java
@@ -1,6 +1,8 @@
 package com.yeem.lamp.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yeem.lamp.entity.AladdinMember;
 import com.yeem.lamp.entity.AladdinService;
 
 import java.io.Serializable;
@@ -9,4 +11,5 @@ import java.util.List;
 public interface IAladdinServiceService extends IService<AladdinService> {
     List<AladdinService> listByMemberId(Long memberId);
     boolean removeByMemberId(Serializable id);
+    IPage<AladdinService> pages(int current, int size);
 }
--- aladdin-lamp/src/main/java/com/yeem/lamp/service/impl/AladdinServiceServiceImpl.java
+++ aladdin-lamp/src/main/java/com/yeem/lamp/service/impl/AladdinServiceServiceImpl.java
@@ -2,13 +2,18 @@ package com.yeem.lamp.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yeem.common.entity.BaseEntity;
 import com.yeem.lamp.config.Constant;
+import com.yeem.lamp.entity.AladdinMember;
 import com.yeem.lamp.entity.AladdinService;
 import com.yeem.lamp.mapper.AladdinServiceMapper;
 import com.yeem.lamp.service.IAladdinServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -16,6 +21,10 @@ import java.util.List;
 
 @Service
 public class AladdinServiceServiceImpl extends ServiceImpl<AladdinServiceMapper, AladdinService> implements IAladdinServiceService {
+
+    @Autowired
+    private AladdinServiceMapper aladdinServiceMapper;
+
     @Override
     public List<AladdinService> list() {
         QueryWrapper<AladdinService> queryWrapper = new QueryWrapper<>();
@@ -41,5 +50,18 @@ public class AladdinServiceServiceImpl extends ServiceImpl<AladdinServiceMapper,
         updateWrapper.set(BaseEntity.BaseField.DELETE_FLAG.getName(), Constant.TRUE_NUMBER);
         updateWrapper.eq("member_id", id);
         return super.update(updateWrapper);
+    }
+
+    @Override
+    public IPage<AladdinService> pages(int current, int size) {
+        QueryWrapper<AladdinService> queryWrapper = new QueryWrapper<>();
+//        if (!StringUtils.isEmpty(email)) {
+//            queryWrapper.like("email", email);
+//        }
+//        if (!StringUtils.isEmpty(wechat)) {
+//            queryWrapper.like("wechat", wechat);
+//        }
+        IPage<AladdinService> page = new Page<>(current, size);
+        return aladdinServiceMapper.selectPage(page, queryWrapper);
     }
 }
