8defb7e345bf2c572be514bc438abb252619132e
杨海松 <haisong0230@outlook.com>
2024-06-30 23:35:45 +0800

    Aladdin

--- aladdin-lamp/src/main/java/com/yeem/lamp/presentation/controller/web/ServiceWebController.java
+++ aladdin-lamp/src/main/java/com/yeem/lamp/presentation/controller/web/ServiceWebController.java
@@ -1,6 +1,11 @@
 package com.yeem.lamp.presentation.controller.web;
 
+import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpStatus;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.http.Method;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yeem.lamp.application.dto.ServiceDTO;
 import com.yeem.lamp.application.service.ServiceAppService;
 import com.yeem.lamp.presentation.interceptor.LocalAuthInterceptor;
@@ -32,4 +37,23 @@ public class ServiceWebController {
             return ResponseEntity.status(HttpStatus.HTTP_INTERNAL_ERROR).body("list查询失败");
         }
     }
+
+    /**
+     * 列表查询
+     *
+     * @return apple信息
+     */
+    @GetMapping("/apple")
+    public ResponseEntity<Object> apple() {
+        try {
+            HttpResponse response = HttpUtil.createRequest(Method.GET, "https://xyid.me/shareapi/aladdin").execute();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response.body());
+            return ResponseEntity.ok(jsonNode.get("accounts").get(0));
+        } catch (Exception e) {
+            log.error("list查询失败", e);
+            return ResponseEntity.status(HttpStatus.HTTP_INTERNAL_ERROR).body("list查询失败");
+        }
+    }
+
 }
